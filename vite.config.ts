cc97058a15243576e127dc1f561f4df6fcad101e
filwas <fwas.corp@gmail.com>
2024-10-04 16:39:32 +0200

    netlify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { installGlobals } from "@remix-run/node";
 import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
+installGlobals();
+
 export default defineConfig({
-  plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    remix({
+      ignoredRouteFiles: ["**/*.css"],
+    }),
+    netlifyPlugin(),
+    tsconfigPaths(),
+  ],
 });
