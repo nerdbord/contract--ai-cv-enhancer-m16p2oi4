0b8feb8b02869532789d3cf919635c105eb2f1dc
filwas <fwas.corp@gmail.com>
2024-10-13 19:10:43 +0200

    cv colors added

--- app/routes/result.tsx
+++ app/routes/result.tsx
@@ -7,6 +7,7 @@ import {
   ReactPortal,
   Key,
   useRef,
+  useState,
 } from "react";
 import CVComponent from "~/components/cvComponent";
 import CV from "~/components/cvComponent";
@@ -16,23 +17,25 @@ import { cvType, resumeType } from "~/types/resume";
 import logo from "/logo.svg";
 import { Button } from "~/components/ui/button";
 import generatePDF from "react-to-pdf";
+import { Colors } from "~/types/color";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const session = await getSession(request);
-  const fineTunedData = session.get("fine-tuned") as resumeType;
+  // const session = await getSession(request);
+  // const fineTunedData = session.get("fine-tuned") as resumeType;
 
-  if (!fineTunedData) {
-    throw new Response("No CV data found", { status: 404 });
-  }
+  // if (!fineTunedData) {
+  //   throw new Response("No CV data found", { status: 404 });
+  // }
 
-  return { fineTunedData };
-  // return json({});
+  // return { fineTunedData };
+  return json({});
 };
 
 export default function Result() {
-  const { fineTunedData } = useLoaderData<typeof loader>();
-  const typedData: cvType = fineTunedData;
-  // const typedData: cvType = mockCVData;
+  const [color, setColor] = useState<Colors>(Colors.Sky);
+  // const { fineTunedData } = useLoaderData<typeof loader>();
+  // const typedData: cvType = fineTunedData;
+  const typedData: cvType = mockCVData;
   const targetRef = useRef(null);
 
   const handleDownload = (
@@ -41,8 +44,32 @@ export default function Result() {
     generatePDF(() => targetRef.current, { filename: "yourCV.pdf" });
   };
 
+  const handleColorChange = (color: Colors) => {
+    setColor(color);
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
+      <div className="flex flex-col mt-12 mr-12">
+        Colors:
+        {(Object.keys(Colors) as Array<keyof typeof Colors>).map(
+          (value, index) => {
+            const colorString = Colors[value] as string;
+            const classString = `w-16 h-16 ${
+              Colors[value] === color
+                ? `border-8 border-${colorString}-100`
+                : ""
+            } rounded-full m-1 bg-${colorString}-50`;
+            return (
+              <div
+                key={index}
+                className={classString}
+                onClick={() => handleColorChange(Colors[value])}
+              ></div>
+            );
+          }
+        )}
+      </div>
       <div className="w-full max-w-[80vh] z-10">
         <img src={logo} alt="Logo" className="absolute top-16 left-24" />
         <div className="w-full flex justify-end mt-24">
@@ -52,11 +79,11 @@ export default function Result() {
             className="bg-cyan-700 hover:bg-cyan-800 focus:bg-cyan-900 mb-6"
             onClick={handleDownload}
           >
-            Print as .pdf
+            Download your CV
           </Button>
         </div>
         <div ref={targetRef}>
-          <CVComponent data={typedData} />
+          <CVComponent data={typedData} color={color} />
         </div>
       </div>
     </div>
---
+++ app/types/color.ts
@@ -0,0 +1,9 @@
+export enum Colors {
+  Sky = "sky",
+  Indigo = "indigo",
+  Fuchsia = "fuchsia",
+  Rose = "rose",
+  Orange = "orange",
+  Amber = "amber",
+  Green = "green",
+}
