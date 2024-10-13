5443a134e73ce4d07d4ed2a0f767f5f49b336fd5
filwas <fwas.corp@gmail.com>
2024-10-13 13:30:37 +0200

    add loaders v1

--- app/routes/parser.tsx
+++ app/routes/parser.tsx
@@ -4,17 +4,18 @@ import type {
   MetaFunction,
 } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useSubmit } from "@remix-run/react";
+import { Form, useNavigate, useSubmit } from "@remix-run/react";
 import { commitSession, getSession, getUserId } from "~/session.server";
 import logo from "/logo.svg";
 import triangles from "/main triangles.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 
 import { transformCVBasedOnOffer } from "~/models/openai.server";
 import { resumeSchema } from "~/types/resume";
 import { getWebsiteText } from "netlify/functions/dataScraper";
+import { Progress } from "~/components/ui/progress";
 
 export const meta: MetaFunction = () => {
   return [
@@ -32,19 +33,23 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export const action: ActionFunction = async ({ request }) => {
   const session = await getSession(request);
+
   const cvData = session.get("cvData");
+
   const formData = await request.formData();
+
   const url = formData.get("url-string");
-  console.log("Action URL: ", url);
 
   const extractedText = await getWebsiteText(url as string);
-  
+
   const finetunedCV = await transformCVBasedOnOffer(cvData, extractedText);
+
   const parsedFineTunedCV = resumeSchema.safeParse(finetunedCV.object).data;
-  console.log("Parsed: ",parsedFineTunedCV);
 
   session.unset("cvData");
+
   await commitSession(session);
+
   session.set("fine-tuned", parsedFineTunedCV);
 
   return redirect("/result", {
@@ -57,6 +62,25 @@ export const action: ActionFunction = async ({ request }) => {
 export default function Parser() {
   const [isLoading, setIsLoading] = useState(false);
   const submit = useSubmit();
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev < 95) {
+          return prev + 5; // Increment progress (you can adjust the speed here)
+        }
+        return prev;
+      });
+    }, 250); // Update progress every 200ms
+
+    return () => clearInterval(interval);
+  }, [isLoading]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    setIsLoading(true);
+    submit(e.currentTarget);
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
@@ -64,36 +88,43 @@ export default function Parser() {
         <img src={logo} alt="Logo" className="absolute top-16 left-24" />
 
         <div className="absolute w-[100vw] h-[100vh] top-0">
-          <Form
-            method="post"
-            className="flex flex-col items-center justify-center align-middle h-full"
-          >
-            <div className="max-w-[80vh] flex flex-col">
-              <label htmlFor="url-string">
-                <h1 className="text-black text-3xl leading-9 pointer-events-none mb-6">
-                  Paste the job offer URL in the field below
-                </h1>
-                <div className="text-gray-700 text-base leading-normal pointer-events-none mb-1.5">
-                  AI will tailor your CV to the job you are applying for
-                </div>
-              </label>
-              <div className="flex flex-col w-full items-end space-x-2">
-                <Input
-                  id="url-string"
-                  type="string"
-                  name="url-string"
-                  placeholder="http/justjoin.it/offer"
-                  className="mb-6"
-                />
-                <Button
-                  type="submit"
-                  className="bg-cyan-700 hover:bg-cyan-800 focus:bg-cyan-900"
-                >
-                  Generate my resume
-                </Button>
-              </div>
+          {isLoading ? (
+            <div className="flex flex-col items-center justify-center align-middle h-full ">
+              <Progress value={progress} className="max-w-[80vh]"></Progress>
             </div>
-          </Form>
+          ) : (
+            <Form
+              method="post"
+              className="flex flex-col items-center justify-center align-middle h-full"
+              onSubmit={handleSubmit}
+            >
+              <div className="max-w-[80vh] flex flex-col">
+                <label htmlFor="url-string">
+                  <h1 className="text-black text-3xl leading-9 pointer-events-none mb-6">
+                    Paste the job offer URL in the field below
+                  </h1>
+                  <div className="text-gray-700 text-base leading-normal pointer-events-none mb-1.5">
+                    AI will tailor your CV to the job you are applying for
+                  </div>
+                </label>
+                <div className="flex flex-col w-full items-end space-x-2">
+                  <Input
+                    id="url-string"
+                    type="string"
+                    name="url-string"
+                    placeholder="http/justjoin.it/offer"
+                    className="mb-6"
+                  />
+                  <Button
+                    type="submit"
+                    className="bg-cyan-700 hover:bg-cyan-800 focus:bg-cyan-900"
+                  >
+                    Generate my resume
+                  </Button>
+                </div>
+              </div>
+            </Form>
+          )}
         </div>
       </div>
       <div className="hidden lg:block flex-1">
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -15,6 +15,7 @@ import { Button } from "~/components/ui/button";
 import { OfficeParserConfig, parseOffice } from "officeparser";
 import path from "path";
 import { promises as fsPromises } from "fs";
+import { Progress } from "~/components/ui/progress";
 
 export const meta: MetaFunction = () => {
   return [
@@ -80,56 +81,68 @@ export const action: ActionFunction = async ({ request }) => {
 
 export default function Upload() {
   const [isLoading, setIsLoading] = useState(false);
+  const [progress, setProgress] = useState(35);
   const submit = useSubmit();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setProgress(85);
+    }, 200);
+    submit(e.currentTarget);
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
       <div className="w-full max-w-3xl">
         <div className="left-24">
           <img src={logo} alt="Logo" className="absolute top-16 left-24" />
-          <Form
-            method="post"
-            encType="multipart/form-data"
-            onChange={(e) => {
-              setIsLoading(true);
-              submit(e.currentTarget);
-            }}
-          >
-            <div className="relative w-full h-[264px] left-24 bg-neutral-50 rounded-lg border border-neutral-400 flex justify-center items-center">
-              <label
-                htmlFor="file-upload"
-                className="w-full h-full flex flex-col justify-center items-center cursor-pointer"
+          {isLoading ? (
+            <Progress value={progress} className="relative left-24"></Progress>
+          ) : (
+            <div>
+              <Form
+                method="post"
+                encType="multipart/form-data"
+                onChange={handleSubmit}
               >
-                <Button
-                  disabled={isLoading}
-                  className="relative w-[188px] h-10 px-4 py-2 bg-cyan-700 hover:bg-cyan-950 focus:bg-cyan-700 rounded-md justify-center items-center gap-2.5 inline-flex z-20"
-                >
-                  <span className="text-white text-base leading-normal">
-                    Upload your resume
-                  </span>
-                  <div className="absolute top-14 text-gray-500 text-base leading-normal pointer-events-none">
-                    or drag and drop file
-                  </div>
-                </Button>
-              </label>
-              <input
-                id="file-upload"
-                accept="application/pdf, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.presentationml.presentation, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.oasis.opendocument.text"
-                type="file"
-                name="file-upload"
-                required
-                className="absolute inset-0 w-full h-full opacity-0 cursor-pointer z-10"
-              />
+                <div className="relative w-full h-[264px] left-24 bg-neutral-50 rounded-lg border border-neutral-400 flex justify-center items-center">
+                  <label
+                    htmlFor="file-upload"
+                    className="w-full h-full flex flex-col justify-center items-center cursor-pointer"
+                  >
+                    <Button
+                      disabled={isLoading}
+                      className="relative w-[188px] h-10 px-4 py-2 bg-cyan-700 hover:bg-cyan-950 focus:bg-cyan-700 rounded-md justify-center items-center gap-2.5 inline-flex z-20"
+                    >
+                      <span className="text-white text-base leading-normal">
+                        Upload your resume
+                      </span>
+                      <div className="absolute top-14 text-gray-500 text-base leading-normal pointer-events-none">
+                        or drag and drop file
+                      </div>
+                    </Button>
+                  </label>
+                  <input
+                    id="file-upload"
+                    accept="application/pdf, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.presentationml.presentation, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.oasis.opendocument.text"
+                    type="file"
+                    name="file-upload"
+                    required
+                    className="absolute inset-0 w-full h-full opacity-0 cursor-pointer z-10"
+                  />
+                </div>
+              </Form>
+              <div className="relative top-2 left-24 flex flex-row justify-between">
+                <span className="text-gray-700 text-xs leading-tight">
+                  Supported formats: PDF, DOCX, ODT, PPT, XLSX
+                </span>
+                <span className="text-gray-700 text-xs font-medium">
+                  Maximum size: 5 MB
+                </span>
+              </div>
             </div>
-          </Form>
-          <div className="relative top-2 left-24 flex flex-row justify-between">
-            <span className="text-gray-700 text-xs leading-tight">
-              Supported formats: PDF, DOCX, ODT, PPT, XLSX
-            </span>
-            <span className="text-gray-700 text-xs font-medium">
-              Maximum size: 5 MB
-            </span>
-          </div>
+          )}
         </div>
       </div>
       <div className="hidden lg:block flex-1">
