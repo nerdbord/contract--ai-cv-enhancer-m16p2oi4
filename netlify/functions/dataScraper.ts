728744459a64a27890d84336a9c503359c5c972e
filwas <fwas.corp@gmail.com>
2024-10-13 10:32:23 +0200

    chrome aws lambda

--- netlify/functions/dataScraper.ts
+++ netlify/functions/dataScraper.ts
@@ -1,14 +1,18 @@
 import puppeteer from "puppeteer-core";
+import chromium from "chrome-aws-lambda";
 
 export async function getWebsiteText(url: string) {
   console.log("CHROME_PATH:", process.env.CHROME_PATH);
   console.log("Received URL: ", url);
 
+  const executablePath = await chromium.executablePath;
+
+  console.log("executablePath: ", executablePath);
+
   const browser = await puppeteer.launch({
-    executablePath:
-      process.env.CHROME_PATH ||
-      process.cwd() + "/node_modules/chromium/lib/chromium/chrome-linux/chrome",
-    headless: true,
+    args: chromium.args,
+    executablePath: executablePath,
+    headless: chromium.headless,
   });
 
   const page = await browser.newPage();
