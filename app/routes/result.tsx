49e86122d7fe5627295c27b28a4d6f71f4ce8b2b
filwas <fwas.corp@gmail.com>
2024-10-13 17:16:33 +0200

    removing ALL mocks :)

--- app/routes/result.tsx
+++ app/routes/result.tsx
@@ -16,21 +16,21 @@ import logo from "/logo.svg";
 import { Button } from "~/components/ui/button";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  // const session = await getSession(request);
-  // const fineTunedData = session.get("fine-tuned") as resumeType;
+  const session = await getSession(request);
+  const fineTunedData = session.get("fine-tuned") as resumeType;
 
-  // if (!fineTunedData) {
-  //   throw new Response("No CV data found", { status: 404 });
-  // }
+  if (!fineTunedData) {
+    throw new Response("No CV data found", { status: 404 });
+  }
 
-  // return { fineTunedData };
-  return json({});
+  return { fineTunedData };
+  // return json({});
 };
 
 export default function Result() {
-  // const { fineTunedData } = useLoaderData<typeof loader>();
-  // const typedData: resumeType = fineTunedData;
-  const typedData: cvType = mockCVData;
+  const { fineTunedData } = useLoaderData<typeof loader>();
+  const typedData: resumeType = fineTunedData;
+  // const typedData: cvType = mockCVData;
   // return <CVComponent data={typedData} />;
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
